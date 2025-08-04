8453cded13693d03ec28c137bd1a9bdca0dfba69
zloth <soyleyendahd@gmail.com>
2025-08-04 23:12:46 +0200

    Add next.config.ts 1754341965012

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config: any) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
+}
+
+export default nextConfig
